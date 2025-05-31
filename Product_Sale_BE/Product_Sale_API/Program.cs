c56360c183f205931aab02e20b59b198506dddcf
Quy <96563375+QuyTurtle12@users.noreply.github.com>
2025-05-31 11:09:49 +0700

    feat: Added code structure & get all products feature

--- Product_Sale_BE/Product_Sale_API/Program.cs
+++ Product_Sale_BE/Product_Sale_API/Program.cs
@@ -1,3 +1,13 @@
+using System.Reflection;
+using BusinessLogic.IServices;
+using BusinessLogic.Services;
+using DataAccess.Entities;
+using DataAccess.IRepositories;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Product_Sale_API.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,50 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+// Get connection string from appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("MyCnn");
+
+// Register DbContext with DI
+builder.Services.AddDbContext<SalesAppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Make all route lowercase
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.LowercaseUrls = true;
+});
+
+// Add Swagger services with XML comments
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Product Sale API",
+        Version = "v1"
+    });
+
+    // Add XML Comments
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+// Register AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Register Repositories and Services
+builder.Services.AddScoped<IUOW, UOW>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
@@ -18,7 +72,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowAllOrigins");
+
 app.UseAuthorization();
+
+//app.UseMiddleware<PermissionHandlingMiddleware>();
+
+app.UseMiddleware<CustomExceptionHandlerMiddleware>();
 
 app.MapControllers();
 
